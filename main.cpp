22ab257ad388fa46b9386d3a1c5e6bcb3713fb7b
大和 宮﨑 <k023g0041@g.neec.ac.jp>
2023-11-08 12:53:01 +0900

    プルリクエストの作成

--- main.cpp
+++ main.cpp
@@ -2,12 +2,29 @@
 
 const char kWindowTitle[] = "LC1B_26_ミヤザキ_ヤマト";
 
+struct Vector2 {
+	float x;
+	float y;
+};
+struct Box {
+	Vector2 pos;
+	Vector2 velocity;
+	Vector2 acseraration;
+	float radius;
+};
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	Box player{
+		{50,300},
+		{5,5},
+		{0,0},
+		32
+	};
+	int scene = 0;
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -24,7 +41,37 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
+		switch (scene)
+		{
+		case 0:
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				player.acseraration = player.velocity;
+			}
+			player.pos.x += player.acseraration.x;
+			if (player.pos.x >= 1230) {
+				player.acseraration = { 0,0 };
+			}
+			if (keys[DIK_RETURN] && !preKeys[DIK_RETURN]) {
+				scene = 1;
+			}
+			break;
+		case 1:
+			if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+				player.acseraration.x += 0.1f;
+			}
+			if (player.acseraration.x != 0) {
+				player.acseraration.x += 0.1f;
+			}
+			player.pos.x += player.acseraration.x;
+			if (player.pos.x >= 1230) {
+				player.acseraration = { 0,0 };
+			}
+			if (keys[DIK_RETURN] && !preKeys[DIK_RETURN]) {
+				scene = 0;
+			}
+			break;
+		}
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,7 +79,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		Novice::DrawEllipse((int)player.pos.x, (int)player.pos.y, (int)player.radius, (int)player.radius, 0.0f, 0xffffffff, kFillModeSolid);
 		///
 		/// ↑描画処理ここまで
 		///
